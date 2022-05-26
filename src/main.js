ee4167c3c307788d9b8d21ab8d9f03a7404ff701
dongfuwinter <d18638188035@163.com>
2022-05-26 10:35:07 +0800

    � update css

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import App from './App.vue'
 import router from './routes'
 import 'normalcss'
 import '@/assets/css/typo.css'
+import '@/assets/css/base.css'
 createApp(App).use(router).mount('#app')
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="login-container">
-    <el-form :model="form">
-      <div class="title-container">
+  <div class="login_container">
+    <div class="login_box">
+    <el-form :model="form" class="login_form">
+      <div class="title_container">
         <h1 class="title">后台管理</h1>
       </div>
       <el-form-item>
-        <el-icon :size="20" class="svg-container">
+        <el-icon :size="20" class="svg_container">
           <Edit />
         </el-icon>
         <el-input v-model="form.name" />
       </el-form-item>
       <el-form-item>
-        <el-icon :size="20" class="svg-container">
+        <el-icon :size="20" class="svg_container">
           <Edit />
         </el-icon>
         <el-input v-model="form.password" />
       </el-form-item>
     </el-form>
+    </div>
+
   </div>
 </template>
 <script setup>
@@ -26,10 +29,56 @@ import { reactive } from "vue";
 // do not use same name with ref
 const form = reactive({
   name: "",
-  password: ""
+  password: "",
 });
 
 const onSubmit = () => {
   console.log("submit!");
 };
 </script>
+<style scoped lang="scss">
+.login_container {
+  box-sizing: unset !important;
+  height: 100%;
+  background-color: #2b4b6b;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.login_box .avatar_box {
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+}
+.login_box .avatar_box img {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: #eee;
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
